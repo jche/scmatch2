10fa06811a9e383e1f8f2ef2a5500f9bf13ef2c3
mengeks <garethalexuni@gmail.com>
2024-01-27 17:46:57 -0500

    Created quarto document for bootstrap analysis

--- sim_analyses.R
+++ sim_analyses.R
@@ -2,10 +2,10 @@
 # full analysis of simulation results
 
 
-METHODS <- c("diff", "onenn", "csm_scm", "cem_avg", "bal1", "bal2", 
+METHODS <- c("diff", "onenn", "csm_scm", "cem_avg", "bal1", "bal2",
              "or_lm", "ps_lm",
              "or_bart", "ps_bart",
-             "aipw1", "tmle1", 
+             "aipw1", "tmle1",
              "aipw2", "tmle2")
 
 # toy sim -----------------------------------------------------------------
@@ -20,8 +20,8 @@ nt <- 100
 f0_sd <- 1
 set.seed(4)
 df <- gen_df_adv(
-  nc=nc, 
-  nt=nt, 
+  nc=nc,
+  nt=nt,
   f0_sd = f0_sd,
   tx_effect_fun = function(X1, X2) {3*X1+3*X2},
   # f0_fun = function(x,y) {abs(x-y)})
@@ -30,7 +30,9 @@ df <- gen_df_adv(
       dmvnorm(mean = c(0.5,0.5),
               sigma = matrix(c(1,0.8,0.8,1), nrow=2)) * 20   # multiply for more slope!
   })
-df %>% 
+
+
+df %>%
   ggplot(aes(x=X1, y=X2, color=Y)) +
   geom_point(aes(pch=Z)) +
   scale_color_continuous(low="blue", high="orange") +
@@ -44,20 +46,20 @@ ggsave("writeup/figures/sim_toy_ex.png", width=4, height=3)
 
 # check rmse stuff --------------------------------------------------------
 
-res_toy %>% 
-  rename(aipw2=aipw3, tmle2=tmle3) %>% 
+res_toy %>%
+  rename(aipw2=aipw3, tmle2=tmle3) %>%
   pivot_longer(diff:aipw2, names_to="method") %>%
-  filter(method %in% METHODS) %>% 
-  filter(method != "onenn", method != "cem_avg") %>% 
+  filter(method %in% METHODS) %>%
+  filter(method != "onenn", method != "cem_avg") %>%
   group_by(method) %>%
   summarize(
     RMSE = sqrt(mean((value-true_ATT)^2)),
     Bias = abs(mean(value-true_ATT)),
     # Bias = mean(value-true_ATT)
-  ) %>% 
-  mutate(method = fct_reorder(method, RMSE, min)) %>% 
-  pivot_longer(c(RMSE, Bias)) %>% 
-  
+  ) %>%
+  mutate(method = fct_reorder(method, RMSE, min)) %>%
+  pivot_longer(c(RMSE, Bias)) %>%
+
   ggplot(aes(x=method)) +
   geom_point(aes(y=value, pch=name), size=1) +
   theme_classic() +
@@ -90,23 +92,23 @@ ggsave("writeup/figures/sim_toy_results.png", width=3.5, height=3.5)
 
 # canonical sims ----------------------------------------------------------
 
-res_kang <- read_csv("sim_canonical_results/kang_spaceship.csv") %>% 
-  bind_rows(read_csv("sim_canonical_results/kang_spaceship2.csv")) %>% 
+res_kang <- read_csv("sim_canonical_results/kang_spaceship.csv") %>%
+  bind_rows(read_csv("sim_canonical_results/kang_spaceship2.csv")) %>%
   mutate(sim = "kang")
-res_hain <- read_csv("sim_canonical_results/hain_spaceship.csv") %>% 
+res_hain <- read_csv("sim_canonical_results/hain_spaceship.csv") %>%
   mutate(sim = "hain")
-res_acic <- read_csv("sim_canonical_results/acic_spaceship.csv") %>% 
+res_acic <- read_csv("sim_canonical_results/acic_spaceship.csv") %>%
   mutate(sim = "acic")
 
-res <- list(res_kang, res_hain, res_acic) %>% 
+res <- list(res_kang, res_hain, res_acic) %>%
   map_dfr(function(d) {
-    d %>% 
+    d %>%
       select(ninf:sim)
   })
 
 # check dropped units -----------------------------------------------------
 
-res %>% 
+res %>%
   ggplot() +
   geom_density(aes(x=ninf)) +
   geom_density(aes(x=ninf_cem), color="red") +
@@ -118,19 +120,19 @@ res %>%
 
 # do ACIC style plots, for all three datasets?
 acic_plot <- function(simname, title="", ylab="", xlab="", legend.position="none") {
-  res %>% 
-    filter(sim == simname) %>% 
-    
+  res %>%
+    filter(sim == simname) %>%
+
     pivot_longer(diff:aipw2, names_to="method") %>%
-    filter(method %in% METHODS) %>% 
+    filter(method %in% METHODS) %>%
     group_by(method) %>%
     summarize(
       RMSE = sqrt(mean((value-true_ATT)^2)),
       Bias = abs(mean(value-true_ATT)),
-    ) %>% 
-    mutate(method = fct_reorder(method, RMSE, min)) %>% 
-    pivot_longer(c(RMSE, Bias)) %>% 
-    
+    ) %>%
+    mutate(method = fct_reorder(method, RMSE, min)) %>%
+    pivot_longer(c(RMSE, Bias)) %>%
+
     ggplot(aes(x=method)) +
     geom_point(aes(y=value, pch=name), size=1) +
     theme_classic() +
@@ -163,8 +165,8 @@ acic_plot("acic", title="test3", ylab="test", xlab="test2")
 
 
 require(patchwork)
-acic_plot("kang", title="Kang & Schafer", ylab="Value", legend.position=c(0.25, 0.75)) + 
-  acic_plot("hain", title="Hainmueller", xlab="Method") + 
+acic_plot("kang", title="Kang & Schafer", ylab="Value", legend.position=c(0.25, 0.75)) +
+  acic_plot("hain", title="Hainmueller", xlab="Method") +
   acic_plot("acic", title="ACIC 2016")
   # plot_layout(guides = "collect")
 ggsave("writeup/figures/sim_results.png", width=8, height=3.5, units="in")
