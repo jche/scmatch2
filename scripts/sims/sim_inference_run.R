a81f584f195123569ba72314ea20e954054139e8
mengeks <garethalexuni@gmail.com>
2024-09-23 11:21:31 -0400

    Make the inference simulation into a loop

--- scripts/sims/sim_inference_run.R
+++ scripts/sims/sim_inference_run.R
@@ -12,54 +12,30 @@ if ( FALSE ){
       R=2,
       prop_nc_unif = 1/3
     )
+  # att_true is the SATT without noise
+  # att_est is the SATT plus noise
 }
 
 
-# Run the simulation for inference ----
-run_sim_inference_A_E <- function(R = 10){
+run_sim_inference_A_E <- function(R = 10) {
+  prop_nc_unif_values <- c(1/3, 2/3, 3/3)
+  deg_overlap_labels <- c("low", "mid", "high")
 
-  # R <- 2; source( "scripts/analysis/boot_CSM_simulation_code.R" )
-  toy_naive_low <-
-    sim_inference_CSM_A_E(
-      dgp_name="toy",
-      att0=F,
-      R=R,
-      # toy_ctr_dist=0.5,
-      prop_nc_unif = 1/3
+  for (i in seq_along(prop_nc_unif_values)) {
+    sim_result <- sim_inference_CSM_A_E(
+      dgp_name = "toy",
+      att0 = FALSE,
+      R = R,
+      prop_nc_unif = prop_nc_unif_values[i]
     )
-  toy_naive_low$deg_overlap <- "low"
-  save_res_to_csv(toy_naive_low,
-                  FNAME = FNAME)
 
+    sim_result$deg_overlap <- deg_overlap_labels[i]
 
-  toy_naive_mid<-
-    sim_inference_CSM_A_E(
-      dgp_name="toy",
-      att0=F,
-      R=R,
-      # toy_ctr_dist=0.3,
-      prop_nc_unif = 2/3
-    )
-  toy_naive_mid$deg_overlap <- "mid"
-  save_res_to_csv(toy_naive_mid,
-                  FNAME = FNAME)
-
-
-  toy_naive_high<-
-    sim_inference_CSM_A_E(
-      dgp_name="toy",
-      att0=F,
-      R=R,
-      # toy_ctr_dist=0.1,
-      prop_nc_unif = 3/3
-    )
-  toy_naive_high$deg_overlap <- "high"
-  save_res_to_csv(toy_naive_high,
-                  FNAME = FNAME)
+    save_res_to_csv(sim_result, FNAME = FNAME)
+  }
 }
-if ( FALSE ){
-  source( "scripts/analysis/boot_CSM_simulation_code.R" )
-}
+
+
 {
   tictoc::tic()
   R = 1000
